6ae37ceb8d727ae2377661832d5a91089e429244
Francy Pineda <francypineda77@gmail.com>
2017-01-12 10:42:05 -0500

    InicioCurso Java VD4

---
+++ HolaMundo/src/paquete/Ejemplos.java
@@ -0,0 +1,15 @@
+package paquete;
+
+public class Ejemplos {
+	
+	public static void main(String[] args) 
+	{
+		for (int contador = 1;contador <= 20; contador++)
+		{
+			contador = contador *2;
+			System.out.println("El contador vale "+contador);
+			System.out.println("Nueva  linea ");
+		}
+	}
+
+}
---
+++ HolaMundo/src/paquete/Main.java
@@ -0,0 +1,53 @@
+//Este progrma corresponde al curso de codigo facilito del video #1 y #2 
+// Realizado por: francy Pineda
+package paquete;
+
+class Main 
+{ //clase principal
+	/**
+	 *short - numeros  cortos
+	 *long - numeros enteros largos
+	 *float - numeros flotantes
+	 *double - numeros dobles
+	 *byte - se maneja en bytes
+	 *char - caracteres
+	 *boolean - boleanos, verdaderos o falsos
+	 *string - cadenas de texto
+	 *		
+	 *short suma;
+			suma = 3+10;
+			int resta = 8-17;
+			long residuo = 9/2;
+			float multiplicacion =2*(15*(-2));
+			double division = 10/3.4;
+			
+			System.out.println("El resultado de la suma es  : " +suma);							//suma 
+			System.out.println("El resultado de la resta es : " +resta);						//resta
+			System.out.println("El resultado de la multiplicacion es : " +multiplicacion);		//resta
+			System.out.println("El resultado de la division es : " +division);					//resta
+			System.out.println("El resultado de la residuo es : " +residuo);					//resta
+			
+	 *
+	 *
+	 *
+	 */
+
+	public static void main(String[] args) 
+		{
+			int var=6;
+			int var2=10;
+			if ((var ==7) ||  (var2>5))
+				{
+					//CODIGO
+					System.out.println("La operacion es verdadera");
+				}
+			else
+				{
+					//CODIGO
+					System.out.println("La operacion es falsa");
+					
+				}
+
+		}
+
+}
