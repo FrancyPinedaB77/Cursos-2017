0a694b88683a332b5f62962e070def4c5195cb24
FRANCY PINEDA77 <FRANCY PINEDA77>
2017-01-16 17:47:13 -0500

    Funcion suma sencilla

--- CursoJavaSript/video9.js
+++ CursoJavaSript/video9.js
@@ -1,16 +1,5 @@
-var arreglo = [20,10,5]; //Almacenanr cualquier valor 
-var arreglo2 = [1,2,3,4,5]; //Almacenanr cualquier valor 
-
-arreglo.push(7);//pone el elemento al final del arreglo
-arreglo.unshift(2);// lo arregla al inicio
-arreglo.pop(); //elimina e ultimo elemento del arreglo
-console.log(arreglo);
-
-
-for (var i=0;i<arreglo.lenght;i++){
-	console.log(arreglo[i]);
+function sumar_numeros(a,b){
+	return a+b ;
 }
 
-for (var i =arreglo2.length;i>=0;i--){
-	console.log(arreglo2[i]);
-}
+console.log(sumar_numeros(2,3));
