0954e2eaa0b3aebfded3016fa82773bb22984c88
FRANCY PINEDA77 <FRANCY PINEDA77>
2017-01-17 10:55:10 -0500

    usando metodo reduce n arreglos con funciones V17

--- CursoJavaSript/video9.js
+++ CursoJavaSript/video9.js
@@ -1,9 +1,30 @@
-// Metodo foreach en arreglos 
+// Metodo Reduce en arreglos
+// 
 
-var numeros=[2,4,5,6,7,9,10];
+var letras=["h","o","l","a"];
 
-numeros.forEach(function(elemento, index,arreglo){
-	arreglo[index]=Math.pow(elemento,2);
+
+console.log(letras.join(""));
+
+var palabra=letras.reduce(function(valor_anterior_retornado,valor_actual,index,arreglo){
+	return valor_anterior_retornado +  valor_actual;
 });
 
-console.log(numeros);
+console.log(palabra);
+
+
+ var numeros =[20,1,11,22];
+
+ var palabra2=numeros.reduce(function(valor_anterior_retornado,valor_actual,index,arreglo){
+	return valor_anterior_retornado +  valor_actual;
+},5);
+
+console.log(palabra2);
+
+var suma =0;
+
+for (var i=0;i<numeros.length;i++){
+	suma+=numeros[i];
+}
+
+console.log(suma);
