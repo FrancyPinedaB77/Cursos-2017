a74e5585456be24768b219b17fabc8bae07e4b18
FRANCY PINEDA77 <FRANCY PINEDA77>
2017-01-17 10:39:49 -0500

    usando metodo forEach en arreglos con funciones v1

--- CursoJavaSript/video9.js
+++ CursoJavaSript/video9.js
@@ -1,20 +1,9 @@
-// Map ES5 Video #15
+// Metodo foreach en arreglos 
 
 var numeros=[2,4,5,6,7,9,10];
 
+numeros.forEach(function(elemento, index,arreglo){
+	arreglo[index]=Math.pow(elemento,2);
+});
 
-cuadrados =numeros.map(function(elemento){return elemento*elemento});
-console.log(cuadrados);
-/*
- var cuadrados=[];
-
-  for (var i= numeros.length-1;i>=0;i--){
-  	var numero=numeros[i];
-  	cuadrados.push(Math.pow(numero,2));
-
-  }
-
-  console.log(cuadrados);
-
-  */
-
+console.log(numeros);
