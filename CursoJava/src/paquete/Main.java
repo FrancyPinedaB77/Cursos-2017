42aba2afaf338114b66a285a1d85122346259ceb
Francy Pineda <francypineda77@gmail.com>
2017-01-24 11:56:39 -0500

    se pasan variables de string a entero

--- CursoJava/src/paquete/Main.java
+++ CursoJava/src/paquete/Main.java
@@ -9,18 +9,19 @@ public class Main {
 	public static void main (String[] args){
 		// Variables de texto en java
 		String texto ="";
+		double x=0;
+		
 		
 		InputStreamReader in = new InputStreamReader(System.in);//Abriendo un canal de datos que recibe  datos desde el exterior
 		BufferedReader buffer = new BufferedReader(in);//Almacena los datos que recje desde el exterior
 		
 		try{
 			texto = buffer.readLine();//Lee el dato del teclado
-		}catch(IOException e){};
+			x = Double.parseDouble(texto);//pasando un texto a enterio
+		}catch(IOException e){
+			System.out.println("se debe ingresa un numero");	
+		};
+		System.out.println(x+5);
 		
-		 texto = texto.concat("aaawertyui"); //.concar para concatenar 
-		 texto = texto.toLowerCase();					//.toLowerCase para pasa a minuscula y toUpperCase
-		 int a = texto.length();						// contar los espacios de la frase
-		System.out.println(texto);
-		System.out.println("La frase tiene "+a+" numero de letras");
 	}
 }
