0c1fec21d51a805824bb4b61bebfeb63574b0b72
Francy Pineda <francypineda77@gmail.com>
2017-01-24 14:07:37 -0500

    Se deja inicializado el servidor con el metodo run V3

--- Flask/hola_mundo.py
+++ Flask/hola_mundo.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__) # nuevo objeto
+
 @app.route('/')       # wrap o un decorador
 def index ():		  # Se usa una funcion
-	return 'Hola mundo'
-app.run() # Ejecuta el servidor por default en el puerto 5000
-
+	return 'Hola mundo parte2 y otros cambios '
+	
+if __name__=='__main__':
+	app.run(debug=True,port=8000) # Ejecuta el servidor por default en el puerto 5000
